f4a8e6eda7071c552ffd258ab107423881ed61f0
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-26 06:11:44 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './i18n.js'
+import './i18n/i18n'; // ✅ correct path
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
